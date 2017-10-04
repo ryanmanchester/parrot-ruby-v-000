f75c3253e8ab5612029c2061f9ca9bab44cbc25c
Ryan Manchester <ryanm17@gmail.com>
2017-10-04 00:01:19 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(" ", greeting="Squawk!")
-  puts "#{" "}"
-  "#{""}"
+def parrot("hello", greeting="Squawk!")
+  puts "hello"
+  "hello"
+
 end
